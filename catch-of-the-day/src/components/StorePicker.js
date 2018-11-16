92d6cc0523aec296b6485cdcf8a3ca9f9421df02
mdroberts74 <mikedroberts88@gmail.com>
2018-11-16 11:22:18 -0600

    Added a button and a text field.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -13,6 +13,8 @@ class StorePicker extends React.Component {
         
         {/*This is how you comment in React. You use curly braces */}
                 <h2>Please Enter A Store</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit"> Visit Store </button>
             </form>
 
 
