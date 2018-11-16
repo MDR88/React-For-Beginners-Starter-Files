1d6370d731f701c6209f9dc24de3dd5a3a88edce
mdroberts74 <mikedroberts88@gmail.com>
2018-11-16 11:13:59 -0600

    Added React Fraction tag. This will allow you to ad children html tags.

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,25 @@
+import React, {Fragment} from 'react';
+
+
+
+class StorePicker extends React.Component {
+
+    render() {
+        return (
+            <Fragment>
+                <p>Fish!</p>
+                <form classname="store-selector">
+                    <h2>Please Enter A Store</h2>
+                </form>
+
+            </Fragment>
+
+        )
+
+
+
+    }
+
+}
+
+export default StorePicker;
